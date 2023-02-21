3a3ad531a7bce8cfd0cfd08045c55e011d58d83b
superman <goodnesskolapo@gmail.com>
2023-02-21 23:30:13 +0100

    started again

---
+++ contracts/swapper.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// 0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0 matic contract address
+
+// 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84 stETH contract address
+
+contract swapper {
+    constructor() {
+        
+    }
+
+    address owner;
+
+
+
+    
+}
