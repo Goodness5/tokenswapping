477f410b18af66a3d2bdc2716a78f18fca954900
superman <goodnesskolapo@gmail.com>
2023-02-22 02:35:15 +0100

    almost done with the logic

--- contracts/swapper.sol
+++ contracts/swapper.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.17;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "./IToken.sol";
 
 // 0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0 matic contract address
 
@@ -10,16 +11,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract swapper {
 
-     address owner;
-     uint256 decimal = 1e18;
-     AggregatorV3Interface private pricefeeddai;
-     AggregatorV3Interface private pricefeedusd;
-     AggregatorV3Interface private pricefeedeth;
-     AggregatorV3Interface private pricefeedbat;
-
-     
-
-     mapping (address => int) pool;
+    address owner;
+    uint256 decimal = 1e18;
+    IERC20 public token1;
+    IERC20 public token2;
+    AggregatorV3Interface private pricefeeddai;
+    //  AggregatorV3Interface private pricefeedusd;
+    AggregatorV3Interface private pricefeedeth;
+    AggregatorV3Interface private pricefeedbat;
 
      
     constructor() {
@@ -30,22 +29,67 @@ contract swapper {
     }
 
 
-       function getLatestPrice (AggregatorV3Interface pricefeed) public view returns (uint80 roundID, int price,uint startedAt,uint timeStamp,
-        uint80 answeredInRound) {
-        (roundID, price,startedAt,timeStamp,answeredInRound) = pricefeed.latestRoundData();
 
+    function getLatestPrice (AggregatorV3Interface pricefeed) public view returns (uint80 roundID, int price,
+        uint  startedAt,uint timeStamp,uint80 answeredInRound) {
+        (roundID, price,startedAt,timeStamp,answeredInRound) = pricefeed.latestRoundData();
     } 
+
+
+ function swapDaiToEth(uint256 _amounttoswap) public {
+    (, int daiPrice, , , ) = getLatestPrice(pricefeeddai);
+    (, int ethPrice, , , ) = getLatestPrice(pricefeedeth);
+    uint256 daiPriceInUsd = uint256(daiPrice);
+    uint256 ethPriceInUsd = uint256(ethPrice);
+    uint256 daiPriceInEth = daiPriceInUsd * decimal / ethPriceInUsd;
+    uint256 amountToReceive = _amounttoswap * daiPriceInEth / decimal;
+    bool deduct = token1.transferFrom(msg.sender, address(this), _amounttoswap);
+    bool pay =  token2.transfer(msg.sender, amountToReceive);
+    require(deduct, "Transfer of token1 failed");
+    require(pay, "Transfer of token2 failed");
+}
+
+
+
+   function swapEthToDai(uint256 _amounttoswap) public {
+    (, int daiPrice, , , ) = getLatestPrice(pricefeeddai);
+    (, int ethPrice, , , ) = getLatestPrice(pricefeedeth);
+    uint256 ethPriceInUsd = uint256(ethPrice);
+    uint256 daiPriceInUsd = uint256(daiPrice);
+    uint256 daiPriceInEth = ethPriceInUsd * decimal / daiPriceInUsd;
+    uint256 amountToReceive = _amounttoswap * daiPriceInEth / decimal;
+    bool deduct = token1.transferFrom(msg.sender, address(this), _amounttoswap);
+    bool pay =  token2.transfer(msg.sender, amountToReceive);
+    require(deduct, "Transfer of token1 failed");
+    require(pay, "Transfer of token2 failed");
+}
+
+   function swapEthToBat(uint256 _amounttoswap) public {
+    (, int batPrice, , , ) = getLatestPrice(pricefeedbat);
+    (, int ethPrice, , , ) = getLatestPrice(pricefeedeth);
+    uint256 ethPriceInUsd = uint256(ethPrice);
+    uint256 batPriceInUsd = uint256(batPrice);
+    uint256 batPriceInEth = ethPriceInUsd * decimal / batPriceInUsd;
+    uint256 amountToReceive = _amounttoswap * batPriceInEth / decimal;
+    bool deduct = token1.transferFrom(msg.sender, address(this), _amounttoswap);
+    bool pay =  token2.transfer(msg.sender, amountToReceive);
+    require(deduct, "Transfer of token1 failed");
+    require(pay, "Transfer of token2 failed");
+}
+ function swapBatToEth(uint256 _amounttoswap) public {
+    (, int batPrice, , , ) = getLatestPrice(pricefeedbat);
+    (, int ethPrice, , , ) = getLatestPrice(pricefeedeth);
+    uint256 batPriceInUsd = uint256(batPrice);
+    uint256 ethPriceInUsd = uint256(ethPrice);
+    uint256 batPriceInEth = batPriceInUsd * decimal / ethPriceInUsd;
+    uint256 amountToReceive = _amounttoswap * batPriceInEth / decimal;
+    bool deduct = token1.transferFrom(msg.sender, address(this), _amounttoswap);
+    bool pay =  token2.transfer(msg.sender, amountToReceive);
+    require(deduct, "Transfer of token1 failed");
+    require(pay, "Transfer of token2 failed");
+}
 
    
 
-        // function swap(uint tokenamount) public {
-       
-
-        // (, int price, , , ) = priceFeed.latestRoundData();
-
-        //  uint token2Amount = (_amount * uint(price)) / (10 ** 18)
-
-        // token1.transferFrom(msg.sender, address(this), token2Amount);
-        // token2.transfer(msg.sender, token2Amount);
-    }
+ }
 
